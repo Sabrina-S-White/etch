6d9040a6c36f80c10f2879f05a90ece5e2c13b73
sabrinaMac <sabrina.scherma@gmail.com>
2022-08-16 16:17:04 -0600

    working on javascript

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
- function createBoard() {
-    for (i = 0; i < 256; i++) {
-        let tile = document.createElement('div');
-    tile.className = 'tile';
-    
-    document.getElementById('container').appendChild(tile);
-    
-    }
-}
 
-window.onload = createBoard();
+ function createBoard(number) {
+    document.getElementById('container').innerHTML = "";
+    for (i = 0; i < number; i++) {
+        let tile = document.createElement('div');
+        tile.className = 'tile';
+        document.getElementById('container').appendChild(tile);
+    }};
+
+window.onload = createBoard(256);
 
 const cells = document.querySelectorAll('div.tile');
+const buttons = document.querySelector('#btn');
+
+function clearGrid () {
+    
+}
+
+buttons.addEventListener('click', function() {
+    let number = prompt('Please select the number of tiles per row you want'); 
+    if (number <= 1) {
+        alert('Please choose a number higher than 1');
+        return;
+    } else {
+        clearGrid();
+        createBoard(number);
+    }});
 
 cells.forEach(cell => {
     cell.addEventListener('mouseover', function () {
         cell.style.backgroundColor = 'blue';
     })
-})
+});
